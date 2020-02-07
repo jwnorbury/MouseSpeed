2ec9976d1ed94d725feecae3e8928468633f865a
James Norbury <jwnorbury@gmail.com>
2020-02-07 20:50:35 +0000

    Initial implementation of mouse speed utility.

---
+++ MouseSpeed/MouseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MouseSpeed
+{
+    public class MouseHandler
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "SystemParametersInfo")]
+        private static extern bool SetMouseSpeed(uint uiAction, uint uiParam, uint speed, uint fWinIni);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "SystemParametersInfo")]
+        private static extern bool GetMouseSpeed(uint uiAction, uint uiParam, ref uint speed, uint fWinIni);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "SystemParametersInfo")]
+        private static extern bool SetMouseAcceleration(uint uiAction, uint uiParam, uint[] mouseInfo, uint fWinIni);
+
+        public static uint GetMouseSpeed()
+        {
+            uint speed = 0;
+            GetMouseSpeed(112, 0, ref speed, 0);
+            return speed;
+        }
+
+        public static void SetMouseSpeed(uint speed)
+        {
+            SetMouseSpeed(113, 0, speed, 0);
+        }
+
+        public static void SetMouseAcceleration(bool enabled)
+        {
+            var mouseInfo = enabled
+                ? new uint[] { 0, 0, 1 }
+                : new uint[] { 0, 0, 0 };
+            SetMouseAcceleration(4, 0, mouseInfo, 0);
+        }
+    }
+}
---
+++ MouseSpeed/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MouseSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                FlipMouseSpeed();
+            }
+            else
+            {
+                (uint speed, bool? acceleration) = GetInput(args);
+                MouseHandler.SetMouseSpeed(speed);
+                if (acceleration.HasValue)
+                {
+                    MouseHandler.SetMouseAcceleration(acceleration.Value);
+                }
+            }
+        }
+
+        private static void FlipMouseSpeed()
+        {
+            var mouseSpeed = MouseHandler.GetMouseSpeed();
+            if (mouseSpeed == 4)
+            {
+                MouseHandler.SetMouseSpeed(10);
+                MouseHandler.SetMouseAcceleration(true);
+            }
+            else
+            {
+                MouseHandler.SetMouseSpeed(4);
+                MouseHandler.SetMouseAcceleration(false);
+            }
+        }
+
+        private static (uint, bool?) GetInput(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                return (GetMouseSpeedInput(args), GetMouseAccelerationInput(args));
+            }
+            else
+            {
+                return (GetMouseSpeedInput(args), null);
+            }
+        }
+
+        private static uint GetMouseSpeedInput(string[] args)
+        {
+            if (!uint.TryParse(args[0], out uint speed))
+            {
+                throw new ArgumentException($"{args[0]} is not a number.");
+            }
+
+            return speed;
+        }
+
+        private static bool GetMouseAccelerationInput(string[] args)
+        {
+            if (!bool.TryParse(args[1], out bool acceleration))
+            {
+                throw new ArgumentException($"{args[1]} is not a bool.");
+            }
+
+            return acceleration;
+        }
+    }
+}
